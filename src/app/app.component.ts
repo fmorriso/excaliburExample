f55f55412f34eff9012b5033c2e0cf7bdbed3981
Fred Morrison <fpmorrison@verizon.net>
2017-03-16 21:10:07 -0400

    build 2017-03-16-21-09-43

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,14 +21,16 @@ export class AppComponent implements OnInit {
     // Create an actor with x position of 150px,
     // y position of 40px from the bottom of the screen,
     // width of 200px, height and a height of 20px
-    const paddle = new ex.Actor(150, game.getHeight() - 40, 200, 20);
+    //
 
-    // Let's give it some color with one of the predefined
+    const paddle = this.createPaddle(game.getHeight()); //new ex.Actor(150, game.getHeight() - 40, 200, 20);
+
+/*    // Let's give it some color with one of the predefined
     // color constants
     paddle.color = ex.Color.Chartreuse;
 
     // Make sure the paddle can participate in collisions, by default excalibur actors do not collide
-    paddle.collisionType = ex.CollisionType.Fixed;
+    paddle.collisionType = ex.CollisionType.Fixed;*/
 
     // `game.add` is the same as calling
     // `game.currentScene.add`
@@ -40,7 +42,30 @@ export class AppComponent implements OnInit {
     });
 
     // Create a ball
-    const ball = new ex.Actor(100, 300, 20, 20);
+    let ball: ex.Actor = this.createBall( game.getWidth());
+
+    // Add the ball to the current scene
+    game.add(ball);
+
+    game.start();
+  }
+
+  private createPaddle(gameHeight : number) : ex.Actor {
+    const paddle = new ex.Actor(150, gameHeight - 40, 200, 20);
+
+    // Let's give it some color with one of the predefined
+    // color constants
+    paddle.color = ex.Color.Chartreuse;
+
+    // Make sure the paddle can participate in collisions, by default excalibur actors do not collide
+    paddle.collisionType = ex.CollisionType.Fixed;
+
+    return paddle;
+  }
+
+  private createBall(gameWidth: number) : ex.Actor{
+
+    let ball: ex.Actor = new ex.Actor(100, 300, 20, 20);
 
     // Set the color
     ball.color = ex.Color.Red;
@@ -53,7 +78,9 @@ export class AppComponent implements OnInit {
 
     // Make the "ball" round
     // NOTE: Draw is passed a rendering context and a delta in milliseconds since the last frame
-    ball.draw = function (ctx, delta) {
+    ball.draw = drawBall;
+
+    function drawBall(ctx, delta) {
       // Optionally call original 'base' method
       // ex.Actor.prototype.draw.call(this, ctx, delta)
 
@@ -75,7 +102,7 @@ export class AppComponent implements OnInit {
 
       // If the ball collides with the right side
       // of the screen reverse the x velocity
-      if (this.pos.x + (this.getWidth() / 2) > game.getWidth()) {
+      if (this.pos.x + (this.getWidth() / 2) > gameWidth) {
         this.vel.x *= -1;
       }
 
@@ -86,9 +113,6 @@ export class AppComponent implements OnInit {
       }
     });
 
-    // Add the ball to the current scene
-    game.add(ball);
-
-    game.start();
+    return ball;
   }
 }
